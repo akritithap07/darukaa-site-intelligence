7ffb009f32b9c06b1b1bfb5a67e250f55329395d
Akriti Thapliyal <akritithapliyal005@gmail.com>
2026-09-18 22:14:23 +0530

    origin changed

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origin_list(),
+    allow_origins=["https://darukaa-site-intelligence.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
